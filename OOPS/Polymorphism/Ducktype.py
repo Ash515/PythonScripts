38f756d4a2e93d75df939bfc486d889c3947816f
Ash515 <aaashwin515@gmail.com>
2021-04-16 15:52:04 +0530

    16-04-01 update

---
+++ OOPS/Polymorphism/Ducktype.py
@@ -0,0 +1,20 @@
+class head_names:
+    def execute():
+        print("IT -> ABC")
+        print("CSE -> cdf")
+
+class lead_names:
+    def execute():
+        print("IT -> xvb")
+        print("CSE -> lok")
+
+class university():
+    def dept_HOD(self,names):
+        names.execute()
+   
+
+
+
+a=university()
+a.dept_HOD(head_names)
+a.dept_HOD(lead_names)
